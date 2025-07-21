c591ca3d8c2c40eddfac4c16a8129dc2e6c5b69b
ry0218 <ry0218@naver.com>
2025-07-21 18:16:29 +0900

    Fix: Admin page 마이그레이션

--- src/app/services/apis/index.ts
+++
@@ -1,5 +0,0 @@
-// API 함수들을 통합 관리
-export * from "./auth";
-export * from "./landing";
-export * from "./guide";
-export { default as apiClient } from "./base";
--- src/app/services/token/index.ts
+++
@@ -1,2 +0,0 @@
-// 토큰 관련 유틸리티들을 쉽게 import할 수 있도록 re-export
-export * from "./tokenUtils";
